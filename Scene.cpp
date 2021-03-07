26e67bfaf302d90dbe1029ed1278796f6ae14e82
cenyc <14007653@qq.com>
2021-03-07 18:21:54 +0800

    新增raymarching

--- Scene.cpp
+++ Scene.cpp
@@ -81,7 +81,7 @@ void Scene::saveGrayImg(string filename) {
     int colorValue = 0;
     
     FILE *fp = fopen(filename.c_str(), "wb");
-    cout<<"1111";
+
     for (y = DR::HEIGHT - 1; y >= 0; y--){
         for (x = 0; x < DR::WIDTH; x++) {
             colorValue = 0;
@@ -95,7 +95,7 @@ void Scene::saveGrayImg(string filename) {
             *p++ = (unsigned char)colorValue;    // B
         }
     }
-    cout<<"222";
+
     svpng(fp, DR::WIDTH, DR::HEIGHT, rgb, 0);
     fclose(fp);
     Utils::msg("Finished Save Gray Img.");
